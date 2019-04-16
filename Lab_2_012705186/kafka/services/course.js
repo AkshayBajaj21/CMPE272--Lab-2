d2cb210049cd10de53c30ccbf81b989c9643e3aa
Akshay <akshay.bajaj@sjsu.edu>
2019-04-16 05:38:05 -0700

    test

--- Lab_2_012705186/kafka/services/course.js
+++ Lab_2_012705186/kafka/services/course.js
@@ -20,6 +20,8 @@ function handle_request(msg, callback) {
     console.log("Skip updated to -5 and new value is"  ,skip)
     }
     let returnObj = {};
+    if(currentuser.role == "faculty"){
+        console.log("entered faculty course info")
     Courses.find({facultyid : currentuser.id}).skip(skip).limit(limit).sort({"courseid" : 1})
     .exec()
     .then(doc => { 
@@ -57,7 +59,47 @@ function handle_request(msg, callback) {
         returnObj.message = "error";
         returnObj.data = "Login";
         callback('err');
-    })    
+    })}
+    else{
+
+        console.log("entered student course info")
+            Courses.find().skip(skip).limit(limit).sort({"courseid" : 1})
+            .exec()
+            .then(doc => { 
+                var courses = [];
+                        for(let i=0;i<doc.length;i++){
+                            let info={};
+                            let studentdetails = [];
+                            info.courseid = doc[i].courseid;
+                            info.coursename = doc[i].coursename;
+                            info.facultyid = doc[i].facultyid;
+                            info.courseterm = doc[i].courseterm;
+                            info.coursedept = doc[i].coursedept;
+                            info.coursedescription = doc[i].coursedescription;
+                            info.courseroom = doc[i].courseroom;
+                            info.coursecapacity = doc[i].coursecapacity;
+                            info.waitlistcapacity = doc[i].waitlistcapacity;
+                          //  console.log("studnet info is ",doc[i].studentinfo)
+                            
+                            info.status = 'enrolled';                
+                           // info.status = doc[i].studentinfo.status;
+                            courses.push(info);
+                            
+                        }
+                       // console.log(courses);
+                        returnObj.courses = courses;
+                        returnObj.message = "success";               
+                        returnObj.data = "Login successfully!";
+                        callback(null,returnObj);
+            })
+            .catch(err => {
+                console.log("Error in course view inmongo db is ",err);
+                returnObj.message = "error";
+                returnObj.data = "Login";
+                callback('err');
+            })
+    
+        }   
 };
 
 exports.handle_request = handle_request;
